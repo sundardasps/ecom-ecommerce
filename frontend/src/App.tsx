a1bb29acc532e48b442d1f5f4a63c45ba8a267a3
Sundardas PS <sundardasps0055@gmaail.com>
2025-09-20 16:55:18 +0530

    fix(import): correct ProtectedRoute path from case-sensitive build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -21,7 +21,8 @@ import OrderManagement from "./components/admin/OrderManagement";
 
 import { Provider } from "react-redux";
 import store from "./redux/store";
-import ProtectedRoute from "./components/common/protectedRoute";
+import ProtectedRoute from "./components/common/ProtectedRoute";
+
 
 
 function App() {
